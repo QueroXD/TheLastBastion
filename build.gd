eb7d572c9082ac2cb35c0a8bb66d3e0e0dc83fc5
Swonk12 <polruizmarin@gmail.com>
2024-11-18 19:14:18 +0100

    Centrar la escena Pop-up y hacer que no se puedan abrir varios Pop-ups a la vez :)

--- build.gd
+++ build.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 # Función que se ejecuta al presionar el botón Cerrar
 func _on_ButtonCerrar_pressed():
 	# Remueve este nodo de la escena principal
@@ -9,3 +10,4 @@ func _on_ButtonCerrar_pressed():
 func _ready():
 	# Utiliza Callable para la conexión en Godot 4
 	$TextureRect/Cerrar.connect("pressed", Callable(self, "_on_ButtonCerrar_pressed"))
+	
