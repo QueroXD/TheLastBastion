eb0092766d86915d1ff8651c356a8975e2e0c52c
Álvaro Quero Ariza <114382675+QueroXD@users.noreply.github.com>
2024-12-15 14:32:39 +0100

    Opacity changes

--- main.gd
+++ main.gd
@@ -3,16 +3,20 @@ extends Node2D
 # Carga la escena Intro
 var intro_scene: PackedScene = preload("res://Intro.tscn")
 var intro_instance: Node = null
+@onready var cameraHud = $Camera2D 
 @onready var backgroundmusic = $BackgroundMusic
 
 func _ready() -> void:
 	# Instancia y añade la escena Intro
+	cameraHud.modulate.a = 0.0  # Inicia con opacidad 0
 	intro_instance = intro_scene.instantiate()
 	add_child(intro_instance)
 	intro_instance.tree_exited.connect(_on_intro_instance_removed)
 
 func _on_intro_instance_removed() -> void:
 	backgroundmusic.play(0.0)
+	var tween = create_tween()
+	tween.tween_property(cameraHud, "modulate:a", 1.0, 2.0)  # Aumenta opacidad en 2 segundos
 
 @onready var particle_system = $CanvasLayer/Snow
 @onready var camera = $Camera2D
